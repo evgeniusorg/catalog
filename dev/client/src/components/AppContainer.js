f9419e6153d350e8b37a43d180bfcf613433d2b7
Eugene Chugunov <eugene@eugenes-mbp.int.nordigy.ru>
2018-06-27 16:14:06 +0300

    initialization

---
+++ dev/client/src/components/AppContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+var FA = require('react-fontawesome')
+
+export class App extends React.Component {
+  render() {
+    return <div>test <FA name="rocket" /></div>
+  } 
+}
